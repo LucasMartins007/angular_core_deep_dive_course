326e91e321153049888085b4a15308727d34ce19
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2022-12-14 15:55:58 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-test',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-15-deep-dive-course';
+
+  data = {
+    title: 'angular 15 deep dive course'
+  }
+
+  onLogoClicked() {
+    alert('Hello Angular');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  } 
 }
